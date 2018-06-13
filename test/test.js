a239befa3fc43a33af7dfaab083ad37b4de88809
brakdag <brakdag@gmail.com>
2018-06-13 09:36:54 -0300

    cambios

--- test/test.js
+++ test/test.js
@@ -6,9 +6,16 @@ describe('Módulo', function() {
     it('Creando instancia de la clase,', function() {
         assert.equal((new dolar).test(), true);
     });
-
-
-
-
   });
+});
+
+describe('Pruebas de funciones', function() {
+    var dol = new dolar;
+    describe('Función de fecha', function() {
+       it('Convertir fecha a cadena ceros',function() {console.log(dol.getUrifromDate(1,1,2003));});
+       it('Convertir fecha a cadena sin ceros',function() {console.log(dol.getUrifromDate(11,11,2003));});
+       it('obtener fecha actual',async function() {console.log(await dol.getDolarHoy());
+    });
+       
+    });    
 });
