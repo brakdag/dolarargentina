a5cb94621eba2e9a1ef6ab9f7035d66d59c12003
brakdag <brakdag@gmail.com>
2018-10-03 00:37:28 -0300

    "se quitó librería async y se agrego resquest para simplificar código"

--- test/test.js
+++ test/test.js
@@ -6,15 +6,14 @@
  **/
 
 const assert = require("assert");
-const async = require("async");
 const dolar =  require("../lib/index");
 const dol = new dolar();
+var bancos = dol.getBanklist;
 
 describe("Bancos", function () {
     this.timeout(15000);
-    it(dol.bank["galicia"].name, async function () { console.log(await dol.getDolarHoy("galicia")) });
-    it(dol.bank["montemar"].name, async function () { console.log(await dol.getDolarHoy("montemar")) });
-    it(dol.bank["provincia"].name, async function () { console.log(await dol.getDolarHoy("provincia")) });
-    it(dol.bank["patagonia"].name, async function () { console.log(await dol.getDolarHoy("patagonia")) });
-    it(dol.bank["santanderrio"].name, async function () { console.log(await dol.getDolarHoy("santanderrio")) });
+    bancos.forEach(banco => {
+    it(dol.bank[banco].name, function () {
+         dol.getDolarHoy(banco).then((res,fail)=>console.log(res)) });        
+    });
 });
