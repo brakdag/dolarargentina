ecde0e9a823f7c1a76b1999ebd2cb98a6236a3e4
brakdag <brakdag@gmail.com>
2018-10-02 22:46:32 -0300

    cambios

--- test/test.js
+++ test/test.js
@@ -1,22 +1,20 @@
+/**
+ * Author:    Gustavo David Ferreyra brakdag@gmail.com
+ * Created:   3.10.2018
+ * MIT license.
+ * dolarargentina library
+ **/
+
 const assert = require("assert");
-const dolar = require("../lib/index");
 const async = require("async");
-var dol = new dolar;
+const dolar =  require("../lib/index");
+const dol = new dolar();
 
-
-describe("Bancos", function(){
+describe("Bancos", function () {
     this.timeout(15000);
- /*   it('Bancos Cargados:', function() {
-        var list = dol.getBanklist();
-        console.log( list);
-    });
-    it(dol.bank["galicia"].name,async function() {console.log(await dol.getDolarHoy("galicia"))});
-    it(dol.bank["montemar"].name,async function() {console.log(await dol.getDolarHoy("montemar"))});
-    it(dol.bank["provincia"].name,async function() {console.log(await dol.getDolarHoy("provincia"))});
-    it(dol.bank["patagonia"].name,async function() {console.log(await dol.getDolarHoy("patagonia"))});
-    it(dol.bank["santanderrio"].name,async function() {console.log(await dol.getDolarHoy("santanderrio"))});
-   */ it(dol.bank["santanderrio"].name,dol.getDolarHoy("santanderrio").then((resp)=>console.log(resp)));
-  
-    
-    //it(dol.bank["bna"].name,async function() {console.log(await dol.getDolarHoy("bna"))});
+    it(dol.bank["galicia"].name, async function () { console.log(await dol.getDolarHoy("galicia")) });
+    it(dol.bank["montemar"].name, async function () { console.log(await dol.getDolarHoy("montemar")) });
+    it(dol.bank["provincia"].name, async function () { console.log(await dol.getDolarHoy("provincia")) });
+    it(dol.bank["patagonia"].name, async function () { console.log(await dol.getDolarHoy("patagonia")) });
+    it(dol.bank["santanderrio"].name, async function () { console.log(await dol.getDolarHoy("santanderrio")) });
 });
